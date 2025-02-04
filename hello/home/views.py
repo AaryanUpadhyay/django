caf147bdb0dc55c066537e85a6830bb297584e3e
AaryanUpadhyay <aaryanupadhyay2803@example.com>
2025-02-04 14:59:13 +0530

    Initial commit

---
+++ hello/home/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    context = {
+        "name":"Aryan"
+    }
+    # return HttpResponse("This is home page!")
+    return render(request,'index.html',context)
+
+def about(request):
+    # return HttpResponse("This is about page!")
+    return render(request,'about.html')
+
+def services(request):
+    # return HttpResponse("This is services page!")
+    return render(request,'services.html')
+
+def contact(request):
+    # return HttpResponse("This is contact us page!")
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, "Your Message has been sent successfully!")
+
+        def __str__(self):
+            return self.name
+    return render(request,'contact.html')
